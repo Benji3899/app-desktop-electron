43866e37b3340dbb79aea74b14c7d2fbd14b2c87
Benjamin EPS <benjamin_eps@hotmail.fr>
2024-04-16 14:02:11 +0200

    backup before start backend & socket server

--- src/components/MessageList.vue
+++ src/components/MessageList.vue
@@ -2,6 +2,10 @@
   <div class="message-list">
     <ul>
       <li v-for="(message, index) in messages" :key="index">
+        <div>
+          <span v-if="message.showDate">{{ messsage.date }}</span> <!-- affiche la date si c'est le premier message de la journée -->
+          <span>{{ message.time }}</span> <!-- affiche l'heure du message -->
+        </div>
         {{ message.content }}
       </li>
     </ul>
--- src/screen/ChatScreen.vue
+++ src/screen/ChatScreen.vue
@@ -21,7 +21,18 @@ export default {
   },
   methods: {
     handleMessageSent(message) {
-      this.messages.push({ content: message }); // Ajoute le message à la liste des messages
+      const now = new Date();
+      const lastMessage = this.messages[this.messages.length - 1];
+      const newMessage = {
+        content: messageContent,
+        time: now.toLocaleTimeString([], {hour: '2-digit', minute: "2-digit" }), //  formatage de l'heure
+        date: now.toLocaleDateString(), // Formatage de la date
+        showDate: false // Par défaut, ne pas afficher la date
+      };
+      if (!lastMessage || newMessage.date !== lastMessage.date) {
+        newMessage.showDate = true; // Afficher la date si c'est le premier message de la journée
+      }
+        this.messages.push({ content: message }); // Ajoute le message à la liste des messages
     }
   }
 };
